52ce093f950642546215820904b836343b189f91
Allenspach Marcel (ma17g047) <ma17g047@submit01.ubelix.unibe.ch>
2022-11-14 11:57:57 +0100

    tried script on ubelix; only executed the first filter

--- scripts/job_dummy_script.sh
+++ scripts/job_dummy_script.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+# SLURM Settings
+#SBATCH --job-name="MIALab"
+#SBATCH --time=10:00:00
+#SBATCH --mem-per-cpu=128G
+#SBATCH --mail-user=marcel.allenspach@students.unibe.ch
+#SBATCH --mail-type=ALL
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
 
-#module load Anaconda3
+
+module load Anaconda3
 eval "$(conda shell.bash hook)"
 
 # Load your environment
@@ -9,8 +18,6 @@ conda activate mialab
 while read -r line
 do 
 	echo "Starts main file with the argument" $line
-	python ../bin/main.py --use_filter=$line
-done < filter.txt
+	srun python3 ../bin/main.py --use_filter=$line
 
-# Run your code
-#run python ../bin/main.py
+done < filter.txt
