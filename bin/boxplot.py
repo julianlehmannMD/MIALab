54385dfb5e2d3ee4ea4f1e9b6a67f64ebb45e519
Julian <julian.lehmann@bluewin.ch>
2022-11-23 09:45:56 +0100

    merge with master

--- bin/boxplot.py
+++ bin/boxplot.py
@@ -19,14 +19,16 @@ def main():
     T1_results = pd.read_csv('mia-result/results_all/Normal/T1_results.csv', sep=';')
     T1_and_T2_results = pd.read_csv('mia-result/results_all/Normal/T1_and_T2_results.csv', sep=';')
     standard_7_results = pd.read_csv('mia-result/results_all/Normal/7_standard_results.csv', sep=';')
+    T1_and_gradients_results = pd.read_csv('mia-result/results_all/Normal/T1_and_Gradients_results.csv', sep=';')
 
     # Assign feature number to new data set
-    data1 = pd.DataFrame(T1_results).assign(Features='T1')
-    data2 = pd.DataFrame(T1_and_T2_results).assign(Features='T1 and T2')
-    data3 = pd.DataFrame(standard_7_results).assign(Features='7 standard')
+    data1 = pd.DataFrame(T1_results).assign(Features='T1 intensity')
+    data2 = pd.DataFrame(T1_and_T2_results).assign(Features='T1 and T2 intensities')
+    data3 = pd.DataFrame(T1_and_gradients_results).assign(Features='T1 intensity and T1&T2 gradients')
+    data4 = pd.DataFrame(standard_7_results).assign(Features='7 standard')
 
     # Concat data sets for boxplot
-    cdf = pd.concat([data1, data2, data3])
+    cdf = pd.concat([data1, data2, data3, data4])
     ax = sns.boxplot(x="LABEL", y="HDRFDST", hue="Features", data=cdf)
     plt.show()
 
