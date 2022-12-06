5e10336da1f6c2e80cb52da0a8bf09e695d2e2b8
Julian <julian.lehmann@bluewin.ch>
2022-12-06 08:12:14 +0100

    almost done

--- bin/boxplot.py
+++ bin/boxplot.py
@@ -34,13 +34,16 @@ def main():
     # ax = sns.boxplot(x="LABEL", y="HDRFDST", hue="Features", data=cdf)
     # plt.show()
 
-    T1_results_summary = pd.read_csv('mia-result/results_all/Summary/T1_results_summary.csv', sep=';')
-    T1_and_T2_results_summary = pd.read_csv('mia-result/results_all/Summary/T1_and_T2_results_summary.csv', sep=';')
-    standard_7_results_summary = pd.read_csv('mia-result/results_all/Summary/7_standard_results_summary.csv', sep=';')
+    _1 = pd.read_csv('mia-result/results_all/Summary/1.csv', sep=';')
+    _2 = pd.read_csv('mia-result/results_all/Summary/2.csv', sep=';')
+    _3 = pd.read_csv('mia-result/results_all/Summary/3.csv', sep=';')
+    _4 = pd.read_csv('mia-result/results_all/Summary/4.csv', sep=';')
+    _5 = pd.read_csv('mia-result/results_all/Summary/5.csv', sep=';')
+    _6 = pd.read_csv('mia-result/results_all/Summary/6.csv', sep=';')
 
     # Evaluate Dice and Housedorf
-    results_dict = {1: T1_results_summary, 2: T1_and_T2_results_summary, 3: standard_7_results_summary}
-    dict_dice_multiplied_by_std_labels = {'T1': {}, '7_standard': {}, 'T1_and_T2': {}}
+    results_dict = {1: _1, 2: _2, 3: _3, 4: _4, 5: _5, 6: _6}
+    dict_dice_multiplied_by_std_labels = {'1': {}, '2': {}, '3': {}, '4': {}, '5': {}, '6': {}}
 
     for i in results_dict:
         data = results_dict[i]
@@ -79,12 +82,17 @@ def main():
             summary_value[np.logical_and(whiteMatter_idx, np.logical_and(dice_idx, std_idx))])
 
         if i == 1:
-            dict_ = 'T1'
+            dict_ = '1'
         elif i == 2:
-            dict_ = 'T1_and_T2'
+            dict_ = '2'
         elif i == 3:
-            dict_ = '7_standard'
-
+            dict_ = '3'
+        elif i == 4:
+            dict_ = '4'
+        elif i == 5:
+            dict_ = '5'
+        elif i == 6:
+            dict_ = '6'
         dict_dice_multiplied_by_std_labels[dict_] = {'Amygdala': amygdala_dice_std_times_mean.item(),
                                                      'GreyMatter': greyMatter_dice_std_times_mean.item(),
                                                      'Hippocampus': hippocampus_dice_std_times_mean.item(),
@@ -92,14 +100,20 @@ def main():
                                                      'WhiteMatter': whiteMatter_dice_std_times_mean.item()}
 
     # plot results
-    plt.plot(range(len(dict_dice_multiplied_by_std_labels['T1'])),
-             list(dict_dice_multiplied_by_std_labels['T1'].values()), label='T1')
-    plt.xticks(range(len(dict_dice_multiplied_by_std_labels['T1'])),
-               list(dict_dice_multiplied_by_std_labels['T1'].keys()))
-    plt.plot(range(len(dict_dice_multiplied_by_std_labels['T1_and_T2'])),
-             list(dict_dice_multiplied_by_std_labels['T1_and_T2'].values()), label='T1 and T2')
-    plt.plot(range(len(dict_dice_multiplied_by_std_labels['7_standard'])),
-             list(dict_dice_multiplied_by_std_labels['7_standard'].values()), label='7 standard')
+    plt.plot(range(len(dict_dice_multiplied_by_std_labels['1'])),
+             list(dict_dice_multiplied_by_std_labels['1'].values()), label='1')
+    plt.xticks(range(len(dict_dice_multiplied_by_std_labels['1'])),
+               list(dict_dice_multiplied_by_std_labels['1'].keys()))
+    plt.plot(range(len(dict_dice_multiplied_by_std_labels['2'])),
+             list(dict_dice_multiplied_by_std_labels['2'].values()), label='2')
+    plt.plot(range(len(dict_dice_multiplied_by_std_labels['3'])),
+             list(dict_dice_multiplied_by_std_labels['3'].values()), label='3')
+    plt.plot(range(len(dict_dice_multiplied_by_std_labels['4'])),
+             list(dict_dice_multiplied_by_std_labels['4'].values()), label='4')
+    plt.plot(range(len(dict_dice_multiplied_by_std_labels['5'])),
+             list(dict_dice_multiplied_by_std_labels['5'].values()), label='5')
+    plt.plot(range(len(dict_dice_multiplied_by_std_labels['6'])),
+             list(dict_dice_multiplied_by_std_labels['6'].values()), label='6')
     plt.ylabel(('(1- Mean Dice)* STD Dice'))
     plt.legend()
     plt.show()
