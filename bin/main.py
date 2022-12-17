b4525c21fd7bae79d1a756a43a0a940e4b8b5a95
Julian <julian.lehmann@bluewin.ch>
2022-12-17 18:52:52 +0100

    domi is here

--- bin/main.py
+++ bin/main.py
@@ -63,11 +63,11 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
                                           futil.BrainImageFilePathGenerator(),
                                           futil.DataDirectoryFilter())
 
-    i_global = 5# change this number
+    i_global = 6# change this number
     robustness_best = float('inf')
     first_flag = 1
 
-    for itr in range(7): # change this number
+    for itr in range(6): # change this number
         # load atlas images
         putil.load_atlas_images(data_atlas_dir)
 
@@ -84,7 +84,7 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
                               'registration_pre': True,
                               'coordinates_feature': True,
                               't1w_sobel_feature': True,
-                              't1w_intensity_feature': False,
+                              't1w_intensity_feature': True,
                               't2w_laplacian_feature': False,
                               't1w_laplacian_feature': False,
                               't2w_intensity_feature': False,
