935e74359c1466b92329471dacaf48c88f7b3834
Julian <julian.lehmann@bluewin.ch>
2022-12-17 19:49:29 +0100

    next round

--- bin/main.py
+++ bin/main.py
@@ -67,7 +67,7 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
     robustness_best = float('inf')
     first_flag = 1
 
-    for itr in range(6): # change this number
+    for itr in range(5): # change this number
         # load atlas images
         putil.load_atlas_images(data_atlas_dir)
 
@@ -85,7 +85,7 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
                               'coordinates_feature': True,
                               't1w_sobel_feature': True,
                               't1w_intensity_feature': True,
-                              't2w_laplacian_feature': False,
+                              't2w_laplacian_feature': True,
                               't1w_laplacian_feature': False,
                               't2w_intensity_feature': False,
                               't1w_gradient_intensity_feature': False,
