a654e4826dd6fd9dbffb492b0910b2401debdfa6
Julian <julian.lehmann@bluewin.ch>
2022-12-17 16:43:38 +0100

    next run

--- bin/main.py
+++ bin/main.py
@@ -63,11 +63,11 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
                                           futil.BrainImageFilePathGenerator(),
                                           futil.DataDirectoryFilter())
 
-    i_global = 4# change this number
+    i_global = 5# change this number
     robustness_best = float('inf')
     first_flag = 1
 
-    for itr in range(8): # change this number
+    for itr in range(7): # change this number
         # load atlas images
         putil.load_atlas_images(data_atlas_dir)
 
@@ -83,13 +83,13 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
                               'normalization_pre': True,
                               'registration_pre': True,
                               'coordinates_feature': True,
+                              't1w_sobel_feature': True,
                               't1w_intensity_feature': False,
                               't2w_laplacian_feature': False,
                               't1w_laplacian_feature': False,
                               't2w_intensity_feature': False,
                               't1w_gradient_intensity_feature': False,
                               't2w_gradient_intensity_feature': False,
-                              't1w_sobel_feature': False,
                               't2w_sobel_feature': False
                               }
 
